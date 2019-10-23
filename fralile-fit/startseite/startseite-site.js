e182954eb889221e9e1b71bf6262264c404724d0
waeaenom <lena.huferath@gmx.de>
2019-10-23 14:33:52 +0200

    Startseite

--- fralile-fit/startseite/startseite-site.js
+++ fralile-fit/startseite/startseite-site.js
@@ -1,1 +1,38 @@
 
+class PageStartseite {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+    }
+
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show() {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("startseite/startseite-site.html");
+        let css = await fetch("startseite/startseite-site.css");
+
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts")
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+        this._renderBoatTiles(pageDom);
+
+        this._app.setPageTitle("Startseite");
+        this._app.setPageCss(css);
+        this._app.setPageHeader(pageDom.querySelector("header"));
+        this._app.setPageContent(pageDom.querySelector("main"));
+    }
+  }
